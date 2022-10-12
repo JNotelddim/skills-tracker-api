65247dff0796df8ff850890979f5b8ad58ab39a6
JNotelddim <JNotelddim@users.noreply.github.com>
2022-10-12 16:19:13 -0700

    Add basic Prisma/User services with nest app controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,37 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Post, Body, Get, Param, Delete } from '@nestjs/common';
+import { UserService } from './user.service';
+import { User as UserModel } from '@prisma/client';
+import { v4 as uuid } from 'uuid';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly userService: UserService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Post('user')
+  async signupUser(
+    @Body() userData: { name?: string; email: string },
+  ): Promise<UserModel> {
+    return this.userService.createUser({
+      ...userData,
+      id: uuid(),
+      salt: Buffer.alloc(64, 1),
+      hash: Buffer.alloc(64, 0),
+      iterations: 0,
+    });
+  }
+
+  @Get('user/:id')
+  async getUser(@Param('id') id: string): Promise<UserModel> {
+    return this.userService.user({ id });
+  }
+
+  @Get('users')
+  async getUsers(): Promise<UserModel[]> {
+    return this.userService.users({});
+  }
+
+  @Delete('user/:id')
+  async deleteUser(@Param('id') id: string): Promise<UserModel> {
+    return this.userService.deleteUser({ id });
   }
 }
