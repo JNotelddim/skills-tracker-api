aa110b68bb9e41db437a6fbd7ece39aeee7e86e5
JNotelddim <JNotelddim@users.noreply.github.com>
2022-10-14 09:13:00 -0700

    Update port to 8080

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const prismaService = app.get(PrismaService);
   await prismaService.enableShutdownHooks(app);
-  await app.listen(3000);
+  await app.listen(8080);
 }
 bootstrap();
